bd663dafc46cdbb9161d7165eea85225ec5e6ee0
Pierret-ha <pierre.tihy@hotmail.fr>
2024-06-20 15:36:33 +0200

    Update solaredge.ts
    Update pug-in zigbee SolarEdge

--- src/devices/solaredge.ts
+++ src/devices/solaredge.ts
@@ -13,3 +13,15 @@ const definitions: Definition[] = [
 
 export default definitions;
 module.exports = definitions;
+const {identify, onOff, electricityMeter} = require('zigbee-herdsman-converters/lib/modernExtend');
+
+const definition = {
+    zigbeeModel: ['SE-S-PLG'],
+    model: 'SE-S-PLG',
+    vendor: 'SolarEdge',
+    description: 'Automatically generated definition',
+    extend: [identify(), onOff({"powerOnBehavior":false}), electricityMeter({"cluster":"metering"})],
+    meta: {},
+};
+
+module.exports = definition;
